1fd36bebe12e4ef7c4b718d3c8c0e3a39497ebeb
Erik Saunier <erik.saunier@gmail.com>
2014-11-17 17:23:17 +0100

    WIP Added phpunit test file

---
+++ tests/MaResidence/Component/ApiClient/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\MaResidence\Component\ApiClient;
+
+use MaResidence\Component\ApiClient\Client;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanGetNews()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
