67e5fe34b7eb04a1d7f1539b203620436f427da5
Erik Saunier <erik.saunier@gmail.com>
2014-12-05 16:03:01 +0100

    Added test file

--- tests/MaResidence/Component/ApiClient/ClientTest.php
+++ tests/MaResidence/Component/ApiClient/ClientTest.php
@@ -3,11 +3,24 @@
 namespace tests\MaResidence\Component\ApiClient;
 
 use MaResidence\Component\ApiClient\Client;
+use Phake;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
     public function testCanGetNews()
     {
         $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+
+    public function testIsAccessTokenExpired()
+    {
+        //$this->markTestIncomplete('This test has not been implemented yet.');
+
+        $session = Phake::mock('Symfony\Component\HttpFoundation\Session\SessionInterface');
+        Phake::when($session)->get("mr_api_client.oauth_token")->thenReturn("WTF");
+
+        $client = new Client($session, '', '', '', '', '', '', "mr_api_client.oauth_token");
+        $this->assertTrue($client->isAccessTokenExpired());
+
     }
 }
